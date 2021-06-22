7815795250b9802f006a44390b78727758147348
dcamposrj <douglas@buzzconsultoria.com>
2021-06-22 14:03:18 -0300

    Base de todos os projetos

---
+++ base projetos/bin/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<meta name="description" content="Controle de Exames Toxicologicos">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title> Controle de Exames Toxicologicos</title>
+		<link rel="stylesheet" href="css/estilo.css">
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
+		<link rel="preconnect" href="https://fonts.gstatic.com">
+		<link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,700;1,300&display=swap" rel="stylesheet">
+		<link rel="icon" href="img/icone.png">
+	</head>
+	<body>
+		<header class="cabecalho">
+			<a href="index.php"><h1 class="logo">Controle de Exames Toxicologicos</h1></a>
+		</header>
